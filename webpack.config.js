c1bb72acb6bad54cdf2fcc1a472f8b9100f21318
Ranisans <ranisans@yahoo.com>
2019-11-25 12:17:26 +0300

    feat: add config files to babel, eslint, gitignore, csscomb, webpack add packages for development

---
+++ webpack.config.js
@@ -0,0 +1,110 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = (env, args) => {
+  const config = {
+    entry: './fancy-weather/src/js/main.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, './dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: [{
+            loader: 'html-loader',
+            options: {
+              minimize: false,
+            },
+          }],
+        },
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader', 'eslint-loader'],
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+
+          use: [
+            'style-loader',
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(woff|woff2|ttf|otf|eot)$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+            },
+          }],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'bundle.css',
+      }),
+      new HtmlWebPackPlugin({
+        template: './codejam-image-api/index.html',
+        filename: './index.html',
+      }),
+      new webpack.LoaderOptionsPlugin({
+        test: /\.js$/,
+        options: {
+          eslint: {
+            configFile: path.resolve(__dirname, '.eslintrc'), // this is my helper for resolving paths
+            cache: false,
+          },
+        },
+      }),
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 3000,
+      stats: 'errors-only',
+      clientLogLevel: 'none',
+    },
+  };
+
+  if (args.mode === 'development') {
+    config.devtool = 'cheap-module-eval-source-map';
+  } else {
+    config.optimization = {
+      minimize: true,
+      minimizer: [new TerserPlugin()],
+    };
+  }
+
+  return config;
+};
