278b55f52536be02cc80cd3a8c533597719663a3
ranisans <ranisans@yahoo.com>
2019-12-10 20:45:03 +0300

    feat: add day of week block

---
+++ fancy-weather/src/gui/elementClasses/dayOfWeekElement.js
@@ -0,0 +1,9 @@
+import TranslatableElement from './translatableElement';
+
+class DayOfWeekElement extends TranslatableElement {
+  constructor(classes) {
+    super('span', classes);
+  }
+}
+
+export default DayOfWeekElement;
