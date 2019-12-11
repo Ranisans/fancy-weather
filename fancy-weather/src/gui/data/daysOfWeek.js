e375c70ce9c91ae7c8d6db8ae80dbc4f12bab7d8
ranisans <ranisans@yahoo.com>
2019-12-11 21:08:57 +0300

    feat: change order in days of week because Date.getDay() - Sunday has 0 position

--- fancy-weather/src/gui/data/daysOfWeek.js
+++ fancy-weather/src/gui/data/daysOfWeek.js
@@ -1,11 +1,11 @@
 const daysOfWeek = [
+  { en: 'sunday', ru: 'воскресенье', by: 'надзеля' },
   { en: 'monday', ru: 'понедельник', by: 'панядзелак' },
   { en: 'tuesday', ru: 'вторник', by: 'ауторак' },
   { en: 'wednesday', ru: 'среда', by: 'серада' },
   { en: 'thursday', ru: 'четверг', by: 'чачвер' },
   { en: 'friday', ru: 'пятница', by: 'пятнiца' },
   { en: 'saturday', ru: 'суббота', by: 'субота' },
-  { en: 'sunday', ru: 'воскресенье', by: 'надзеля' },
 ];
 
 export default daysOfWeek;
