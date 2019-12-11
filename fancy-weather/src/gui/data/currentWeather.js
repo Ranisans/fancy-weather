30068db2be666d3b8af0161940553e66c60a8b49
ranisans <ranisans@yahoo.com>
2019-12-11 20:24:51 +0300

    feat: add current weather dict

---
+++ fancy-weather/src/gui/data/currentWeather.js
@@ -0,0 +1,17 @@
+const weatherType = {
+  Clear: { en: 'clear', ru: 'ясно', by: 'ясна' },
+  Clouds: { en: 'clouds', ru: 'облачно', by: 'воблачна' },
+  Drizzle: { en: 'drizzle', ru: 'мелкий дождь', by: 'імжа' },
+  Rain: { en: 'rain', ru: 'ясно', by: 'дождж' },
+  Thunderstorm: { en: 'thunderstorm', ru: 'ясно', by: 'навальніца' },
+};
+
+const feelsLike = { en: 'feels like', ru: 'по ощущению', by: 'адчуваецца як' };
+
+const wind = { en: 'wind', ru: 'ветер', by: 'вецер' };
+
+const humidity = { en: 'humidity', ru: 'влажность', by: 'вільготнасць' };
+
+export {
+  weatherType, feelsLike, wind, humidity,
+};
