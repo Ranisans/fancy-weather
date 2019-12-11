660e14478562c34f97161ce0c043eb2a80f5983a
ranisans <ranisans@yahoo.com>
2019-12-11 20:31:14 +0300

    feat: add search element dict

---
+++ fancy-weather/src/gui/data/searchElements.js
@@ -0,0 +1,5 @@
+const placeholder = { en: 'Search city or ZIP', ru: 'Поиск города', by: 'Пошук горада' };
+
+const search = { en: 'search', ru: 'поиск', by: 'пошук' };
+
+export { placeholder, search };
