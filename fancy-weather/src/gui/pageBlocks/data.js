2a36fe8e23c9e70d6fd6c0f6844bf8c41daa996b
Ranisans <ranisans@yahoo.com>
2019-12-12 17:01:00 +0300

    feat: add value for testing weather blocks

---
+++ fancy-weather/src/gui/pageBlocks/data.js
@@ -0,0 +1,34 @@
+const data = {
+  dt: 1576162800,
+  main: {
+    temp: 1.89,
+    feels_like: -2.64,
+    temp_min: 1.2,
+    temp_max: 1.89,
+    pressure: 1023,
+    sea_level: 1023,
+    grnd_level: 1001,
+    humidity: 97,
+    temp_kf: 0.69,
+  },
+  weather: [
+    {
+      id: 804,
+      main: 'Clouds',
+      description: 'overcast clouds',
+      icon: '04n',
+    },
+  ],
+  clouds: {
+    all: 100,
+  },
+  wind: {
+    speed: 3.96,
+    deg: 186,
+  },
+  sys: {
+    pod: 'n',
+  },
+  dt_txt: '2019-12-12 15:00:00',
+};
+export default data;
