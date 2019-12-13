d9490a99543c176ce877b40fc50ed926d78ba565
Ranisans <ranisans@yahoo.com>
2019-12-13 11:10:02 +0300

    feat: add styles to future weather block

--- fancy-weather/src/gui/pageBlocks/weatherBlock.js
+++ fancy-weather/src/gui/pageBlocks/weatherBlock.js
@@ -12,15 +12,15 @@ class FutureWeatherBlock extends WeatherBlock {
     const block = createElement('div', [baseClass, 'weather_block']);
     mainBlock.appendChild(block);
 
-    this.dayOfWeek = new DayOfWeekElement([`${baseClass}-day_of_week`, 'day_of_week']);
+    this.dayOfWeek = new DayOfWeekElement(['weather_block-day_of_week']);
     this.dayOfWeek.setValue(turn);
     block.appendChild(this.dayOfWeek.element);
     this.translatableElements.push(this.dayOfWeek);
 
-    this.temp = new TemperatureElement([`${baseClass}-temp`, 'future_temp']);
+    this.temp = new TemperatureElement(['weather_block-temp']);
     block.appendChild(this.temp.element);
 
-    this.weatherPict = createElement('div', [`${baseClass}-pict`, 'future_weather_type']);
+    this.weatherPict = createElement('div', ['weather_block-pict']);
     block.appendChild(this.weatherPict);
   }
 
