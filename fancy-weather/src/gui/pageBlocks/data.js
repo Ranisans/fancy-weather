d73477d4b73126f1dbc08db64b2e7ada019d5727
Ranisans <ranisans@yahoo.com>
2019-12-13 15:59:02 +0300

    feat: add test data to position

--- fancy-weather/src/gui/pageBlocks/data.js
+++ fancy-weather/src/gui/pageBlocks/data.js
@@ -31,4 +31,361 @@ const data = {
   },
   dt_txt: '2019-12-12 15:00:00',
 };
-export default data;
+
+const position = {
+  documentation: 'https://opencagedata.com/api',
+  licenses: [
+    {
+      name: 'see attribution guide',
+      url: 'https://opencagedata.com/credits',
+    },
+  ],
+  rate: {
+    limit: 2500,
+    remaining: 2394,
+    reset: 1576281600,
+  },
+  results: [
+    {
+      bounds: {
+        northeast: {
+          lat: 53.9717897,
+          lng: 28.0608997,
+        },
+        southwest: {
+          lat: 53.793847,
+          lng: 27.3740176,
+        },
+      },
+      components: {
+        'ISO_3166-1_alpha-2': 'BY',
+        'ISO_3166-1_alpha-3': 'BLR',
+        _type: 'state',
+        continent: 'Europe',
+        country: 'Беларусь',
+        country_code: 'by',
+        state: 'Мінск',
+      },
+      confidence: 3,
+      formatted: 'Мінск, Беларусь',
+      geometry: {
+        lat: 53.902334,
+        lng: 27.5618791,
+      },
+    },
+    {
+      bounds: {
+        northeast: {
+          lat: -3.9808076,
+          lng: -79.1963967,
+        },
+        southwest: {
+          lat: -3.9808378,
+          lng: -79.1968688,
+        },
+      },
+      components: {
+        'ISO_3166-1_alpha-2': 'EC',
+        'ISO_3166-1_alpha-3': 'ECU',
+        _type: 'road',
+        city: 'Loja',
+        continent: 'South America',
+        country: 'Эквадор',
+        country_code: 'ec',
+        county: 'Loja',
+        neighbourhood: 'Nueva Granada',
+        postcode: '110107',
+        road: 'Minsk',
+        road_type: 'residential',
+        state: 'Loja',
+        state_code: 'L',
+      },
+      confidence: 9,
+      formatted: 'Minsk, 110107, Loja, Эквадор',
+      geometry: {
+        lat: -3.9808378,
+        lng: -79.1968247,
+      },
+    },
+    {
+      bounds: {
+        northeast: {
+          lat: 52.388996,
+          lng: 13.0646144,
+        },
+        southwest: {
+          lat: 52.388395,
+          lng: 13.0637686,
+        },
+      },
+      components: {
+        'ISO_3166-1_alpha-2': 'DE',
+        'ISO_3166-1_alpha-3': 'DEU',
+        _type: 'building',
+        building: 'Minsk',
+        city: 'Potsdam',
+        continent: 'Europe',
+        country: 'Германія',
+        country_code: 'de',
+        political_union: 'European Union',
+        postcode: '14473',
+        road: 'Max-Planck-Straße',
+        state: 'Брандэнбург',
+        suburb: 'Südliche Innenstadt',
+      },
+      confidence: 10,
+      formatted: 'Minsk, Max-Planck-Straße, 14473 Potsdam, Германія',
+      geometry: {
+        lat: 52.3887047,
+        lng: 13.0642087,
+      },
+    },
+    {
+      bounds: {
+        northeast: {
+          lat: 16.0483318,
+          lng: 108.2472624,
+        },
+        southwest: {
+          lat: 16.0482318,
+          lng: 108.2471624,
+        },
+      },
+      components: {
+        'ISO_3166-1_alpha-2': 'VN',
+        'ISO_3166-1_alpha-3': 'VNM',
+        _type: 'bar',
+        bar: 'Minsk',
+        continent: 'Asia',
+        country: "В'етнам",
+        country_code: 'vn',
+        county: 'Ngũ Hành Sơn',
+        postcode: '55000',
+        road: 'Đường Ngô Thì Sĩ',
+        state: 'Đà Nẵng',
+      },
+      confidence: 9,
+      formatted: "Minsk, Đường Ngô Thì Sĩ, Đà Nẵng 55000, В'етнам",
+      geometry: {
+        lat: 16.0482818,
+        lng: 108.2472124,
+      },
+    },
+    {
+      bounds: {
+        northeast: {
+          lat: 16.0482599,
+          lng: 108.2471914,
+        },
+        southwest: {
+          lat: 16.0481599,
+          lng: 108.2470914,
+        },
+      },
+      components: {
+        'ISO_3166-1_alpha-2': 'VN',
+        'ISO_3166-1_alpha-3': 'VNM',
+        _type: 'bar',
+        bar: 'Minsk',
+        continent: 'Asia',
+        country: "В'етнам",
+        country_code: 'vn',
+        county: 'Ngũ Hành Sơn',
+        postcode: '55000',
+        road: 'Đường An Thượng 3',
+        state: 'Đà Nẵng',
+      },
+      confidence: 9,
+      formatted: "Minsk, Đường An Thượng 3, Đà Nẵng 55000, В'етнам",
+      geometry: {
+        lat: 16.0482099,
+        lng: 108.2471414,
+      },
+    },
+    {
+      bounds: {
+        northeast: {
+          lat: 42.7227445,
+          lng: 23.2693328,
+        },
+        southwest: {
+          lat: 42.7216249,
+          lng: 23.2681506,
+        },
+      },
+      components: {
+        'ISO_3166-1_alpha-2': 'BG',
+        'ISO_3166-1_alpha-3': 'BGR',
+        _type: 'road',
+        city: 'район Люлин',
+        continent: 'Europe',
+        country: 'Балга́рыя',
+        country_code: 'bg',
+        county: 'Столична',
+        neighbourhood: 'кв. Модерно предградие',
+        political_union: 'European Union',
+        postcode: '1324',
+        road: 'Минск',
+        road_type: 'residential',
+        state: 'София-град',
+      },
+      confidence: 9,
+      formatted: 'Минск, 1324 район Люлин, Балга́рыя',
+      geometry: {
+        lat: 42.7227445,
+        lng: 23.2693328,
+      },
+    },
+    {
+      bounds: {
+        northeast: {
+          lat: 42.6087287,
+          lng: 23.1000805,
+        },
+        southwest: {
+          lat: 42.6086032,
+          lng: 23.097554,
+        },
+      },
+      components: {
+        'ISO_3166-1_alpha-2': 'BG',
+        'ISO_3166-1_alpha-3': 'BGR',
+        _type: 'road',
+        continent: 'Europe',
+        country: 'Балга́рыя',
+        country_code: 'bg',
+        county: 'Перник',
+        political_union: 'European Union',
+        residential: 'кв.Изток',
+        road: 'Минск',
+        road_type: 'residential',
+        state: 'Перник',
+        suburb: 'кв. Мошино',
+        village: 'Големо Бучино',
+      },
+      confidence: 9,
+      formatted: 'Минск, кв. Мошино, Големо Бучино, Балга́рыя',
+      geometry: {
+        lat: 42.6086625,
+        lng: 23.0989321,
+      },
+    },
+    {
+      bounds: {
+        northeast: {
+          lat: 42.6086032,
+          lng: 23.1035912,
+        },
+        southwest: {
+          lat: 42.6077945,
+          lng: 23.1000409,
+        },
+      },
+      components: {
+        'ISO_3166-1_alpha-2': 'BG',
+        'ISO_3166-1_alpha-3': 'BGR',
+        _type: 'road',
+        continent: 'Europe',
+        country: 'Балга́рыя',
+        country_code: 'bg',
+        county: 'Перник',
+        political_union: 'European Union',
+        residential: 'кв.Изток',
+        road: 'Минск',
+        road_type: 'residential',
+        state: 'Перник',
+        suburb: 'кв. Изток',
+        village: 'Големо Бучино',
+      },
+      confidence: 9,
+      formatted: 'Минск, кв. Изток, Големо Бучино, Балга́рыя',
+      geometry: {
+        lat: 42.6080765,
+        lng: 23.1011377,
+      },
+    },
+    {
+      bounds: {
+        northeast: {
+          lat: 45.1882736,
+          lng: 5.7188622,
+        },
+        southwest: {
+          lat: 45.1881736,
+          lng: 5.7187622,
+        },
+      },
+      components: {
+        'ISO_3166-1_alpha-2': 'FR',
+        'ISO_3166-1_alpha-3': 'FRA',
+        _type: 'nightclub',
+        city: 'Грэнобль',
+        continent: 'Europe',
+        country: 'Францыя',
+        country_code: 'fr',
+        county: 'Grenoble',
+        footway: 'Cours Jean Jaurès',
+        house_number: '44',
+        neighbourhood: "L'Estacade",
+        nightclub: 'Blue Night',
+        political_union: 'European Union',
+        postcode: '38000',
+        state: 'Auvergne-Rhône-Alpes',
+        state_code: 'ARA',
+        suburb: 'Secteur 1',
+      },
+      confidence: 9,
+      formatted: 'Blue Night, 44 Cours Jean Jaurès, 38000 Грэнобль, Францыя',
+      geometry: {
+        lat: 45.1882236,
+        lng: 5.7188122,
+      },
+    },
+    {
+      bounds: {
+        northeast: {
+          lat: 44.6155007,
+          lng: 33.5246215,
+        },
+        southwest: {
+          lat: 44.6154007,
+          lng: 33.5245215,
+        },
+      },
+      components: {
+        'ISO_3166-1_alpha-2': 'RU',
+        'ISO_3166-1_alpha-3': 'RUS',
+        _type: 'memorial',
+        city: 'Севастополь',
+        continent: 'Europe',
+        country_code: 'ru',
+        footway: 'Аллея Городов-Героев',
+        memorial: 'Мінск',
+        postcode: '299000-299699',
+        state: 'ЮФО',
+      },
+      confidence: 9,
+      formatted: 'Мінск, Аллея Городов-Героев, Севастополь, ЮФО, 299000-299699',
+      geometry: {
+        lat: 44.6154507,
+        lng: 33.5245715,
+      },
+    },
+  ],
+  status: {
+    code: 200,
+    message: 'OK',
+  },
+  stay_informed: {
+    blog: 'https://blog.opencagedata.com',
+    twitter: 'https://twitter.com/opencagedata',
+  },
+  thanks: 'For using an OpenCage API',
+  timestamp: {
+    created_http: 'Fri, 13 Dec 2019 12:32:29 GMT',
+    created_unix: 1576240349,
+  },
+  total_results: 10,
+};
+
+export { data, position };
