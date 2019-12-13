f104d2178e2e18bf500b517d43b3dd325429672b
Ranisans <ranisans@yahoo.com>
2019-12-13 12:33:30 +0300

    feat: add spaces to map position dict

--- fancy-weather/src/gui/data/position.js
+++ fancy-weather/src/gui/data/position.js
@@ -1,5 +1,5 @@
-const latitude = { en: 'latitude', ru: 'широта', by: 'шыраты' };
+const latitude = { en: 'latitude: ', ru: 'широта: ', by: 'шырата: ' };
 
-const longitude = { en: 'longitude', ru: 'долгота', by: 'даўгата' };
+const longitude = { en: 'longitude: ', ru: 'долгота: ', by: 'даўгата: ' };
 
 export { latitude, longitude };
