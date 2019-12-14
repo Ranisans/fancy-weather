679b1b2b672b955a74956c8b97608ebe8cdadbc2
ranisans <ranisans@yahoo.com>
2019-12-14 12:14:57 +0300

    refact: remove temp code

--- fancy-weather/src/gui/index.js
+++ fancy-weather/src/gui/index.js
@@ -6,35 +6,23 @@ import createFutureWeatherBlock from './pageBlocks/futureWeather';
 
 import '../../assets/scss/main.scss';
 
-import { weatherBlocksClasses, blocksName } from './constants';
-import { data, position } from './pageBlocks/data';
+import { blocksName } from './constants';
 import DatePositionBlock from './pageBlocks/datePositionBlock';
 
 
 const createGui = () => {
   const main = createElement('main', ['main']);
   document.body.appendChild(main);
-  const language = 'en';
 
   const menuBlock = new MenuBlock(main);
-  menuBlock.translate(language);
 
   const dateBlock = new DatePositionBlock(main);
-  dateBlock.setPosition(position);
-  dateBlock.translate(language);
 
   const currentWeatherBlock = new CurrentWeather(main);
-  currentWeatherBlock.setData(data);
-  currentWeatherBlock.translate(language);
 
   const mapBlock = new MapBlock(main);
-  mapBlock.translate(language);
 
   const futureWeatherBlock = createFutureWeatherBlock(main);
-  weatherBlocksClasses.forEach((element) => {
-    futureWeatherBlock[element].setData(data);
-    futureWeatherBlock[element].translate(language);
-  });
 
   return {
     [blocksName[0]]: menuBlock,
