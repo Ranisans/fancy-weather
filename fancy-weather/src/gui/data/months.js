a8d666874fdc361939a24c414d56d6823b3b3fae
ranisans <ranisans@yahoo.com>
2019-12-15 16:51:12 +0300

    feat: set date and time to DatePosition block

---
+++ fancy-weather/src/gui/data/months.js
@@ -0,0 +1,16 @@
+const months = [
+  { en: 'January', ru: 'Январь', by: 'Студзень' },
+  { en: 'February', ru: 'Февраль', by: 'Люты' },
+  { en: 'March', ru: 'Март', by: 'Сакавік' },
+  { en: 'April', ru: 'Апрель', by: 'Красавік' },
+  { en: 'May', ru: 'Май', by: 'Травень' },
+  { en: 'June', ru: 'Июнь', by: 'Чэрвень' },
+  { en: 'July', ru: 'Июль', by: 'Ліпень' },
+  { en: 'August', ru: 'Август', by: 'Жнівень' },
+  { en: 'September', ru: 'Сентябрь', by: 'Верасень' },
+  { en: 'October', ru: 'Октября', by: 'Кастрычнік' },
+  { en: 'November', ru: 'Ноябрь', by: 'Лістапад' },
+  { en: 'December', ru: 'Декабрь', by: 'Снежань' },
+];
+
+export default months;
--- fancy-weather/src/gui/pageBlocks/datePositionBlock.js
+++ fancy-weather/src/gui/pageBlocks/datePositionBlock.js
@@ -2,11 +2,14 @@ import TranslatableBlock from './AbstractClasses/TranslateBlockClass';
 import createElement from '../elementClasses/createElement';
 import TranslatableElement from '../elementClasses/translatableElement';
 import DayOfWeekElement from '../elementClasses/dayOfWeekElement';
+import months from '../data/months';
 
 class DatePositionBlock extends TranslatableBlock {
   constructor(main) {
     super();
+    this.minute = 60000;
     this.translatableElements = [];
+    this.updateClock = setInterval(() => { }, this.minute);
 
     const block = createElement('div', ['date_position']);
     main.appendChild(block);
@@ -17,30 +20,42 @@ class DatePositionBlock extends TranslatableBlock {
     const dateBlock = createElement('p', ['date_block']);
     block.appendChild(dateBlock);
 
-    this.dayOfWeek = new DayOfWeekElement(['date_block-month']);
+    this.dayOfWeek = new DayOfWeekElement(['date_block-day_of_week']);
     dateBlock.appendChild(this.dayOfWeek.element);
     this.dayOfWeek.setValue(0);
     this.translatableElements.push(this.dayOfWeek);
 
-    this.date = createElement('span', ['date_block-date']);
-    dateBlock.appendChild(this.date);
+    this.day = createElement('span', ['date_block-day']);
+    dateBlock.appendChild(this.day);
 
     this.month = new TranslatableElement('span', ['date_block-month']);
     dateBlock.appendChild(this.month.element);
-    // this.translatableElements.push(this.month);
+    this.translatableElements.push(this.month);
 
     this.time = createElement('span', ['date_block-time']);
     dateBlock.appendChild(this.time);
   }
 
-  setPosition(data) {
-    this.position.innerText = data.results[0].formatted;
+  updateTime() {
+    this.day.innerText = this.date.getDate();
+    this.month.setValue(months[this.date.getMonth()]);
+    this.time.innerText = `${this.date.toLocaleTimeString('ru-RU', { timeStyle: 'short' })}`;
   }
 
-  setDate(data) {
-    console.log('TCL: DatePositionBlock -> setDate -> data', data);
-    // need send data to elements
-    this.date.innerText = 13;
+  setData({ date, position }) {
+    this.date = date;
+    this.position.innerText = position;
+
+    this.updateTime(date);
+
+    clearInterval(this.updateClock);
+
+    this.updateClock = setInterval(() => {
+      this.date = new Date(this.date.getTime() + this.minute);
+      this.updateTime();
+    }, this.minute);
+
+    this.dayOfWeek.updateDayOfWeek(date);
   }
 }
 
