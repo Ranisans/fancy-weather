deb4e47540117654567c8f9b5d27f8709757bcbf
ranisans <ranisans@yahoo.com>
2019-12-15 18:05:36 +0300

    feat: add unsplash key

--- fancy-weather/src/SecretAPIKeys.js
+++ fancy-weather/src/SecretAPIKeys.js
@@ -2,7 +2,8 @@ const timezonedb = 'D3FOCJWIW0AA';
 const openweathermap = '0e41f9c19c0113106e8f6bc32a945991';
 const mapbox = 'pk.eyJ1IjoicmFuaXNhbnMiLCJhIjoiY2s0NXF4M3ZxMGI2cTNtbGY0YXZ6andvcCJ9.ddyOWjJVXzJg9vFIte03-w';
 const opencagedata = '3f190d66705e485c98fb1f0bedf30cf3';
+const unsplash = '6bd15976ff4ddf4cb3852c0590c5c832504ad1983052f720154bc0e500bc03f2';
 
 export {
-  timezonedb, openweathermap, mapbox, opencagedata,
+  timezonedb, openweathermap, mapbox, opencagedata, unsplash,
 };
