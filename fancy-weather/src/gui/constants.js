c4b67d3f86d412ac82efb0c9d7e09cf1a8357d52
ranisans <ranisans@yahoo.com>
2019-12-15 13:18:52 +0300

    feat: add constant to language

--- fancy-weather/src/gui/constants.js
+++ fancy-weather/src/gui/constants.js
@@ -3,4 +3,9 @@ const measuringScale = { C: 'C', F: 'F' };
 const weatherBlocksClasses = ['first_day', 'second_day', 'third_day'];
 
 const blocksName = ['menuBlock', 'dateBlock', 'currentWeatherBlock', 'mapBlock', 'futureWeatherBlock'];
-export { measuringScale, weatherBlocksClasses, blocksName };
+
+const languageCode = { en: 'en', ru: 'ru', by: 'by' };
+
+export {
+  measuringScale, weatherBlocksClasses, blocksName, languageCode,
+};
