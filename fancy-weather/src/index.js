ea3e686e8266716e3ed966d911cbe20981e7fcc1
ranisans <ranisans@yahoo.com>
2019-12-15 11:41:41 +0300

    feat: add sending gui handler to eventListener

--- fancy-weather/src/index.js
+++ fancy-weather/src/index.js
@@ -1,7 +1,13 @@
 import createGui from './gui/index';
+import BlocksHandler from './logic/BlocksHandler';
 
-const init = () => {
-  createGui();
+import eventListenerInit from './logic/eventListener';
+
+const init = async () => {
+  const blocks = createGui();
+  const blockHandler = new BlocksHandler(blocks);
+
+  await eventListenerInit(blockHandler);
 };
 
 init();
