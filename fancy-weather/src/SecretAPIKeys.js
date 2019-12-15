e22f501f1f84276aa92d3dfcfdfc168a5e9b32c5
ranisans <ranisans@yahoo.com>
2019-12-15 13:16:58 +0300

    feat: add geocoding

--- fancy-weather/src/SecretAPIKeys.js
+++ fancy-weather/src/SecretAPIKeys.js
@@ -1,5 +1,8 @@
 const timezonedb = 'D3FOCJWIW0AA';
 const openweathermap = '0e41f9c19c0113106e8f6bc32a945991';
 const mapbox = 'pk.eyJ1IjoicmFuaXNhbnMiLCJhIjoiY2s0NXF4M3ZxMGI2cTNtbGY0YXZ6andvcCJ9.ddyOWjJVXzJg9vFIte03-w';
+const opencagedata = '3f190d66705e485c98fb1f0bedf30cf3';
 
-export { timezonedb, openweathermap, mapbox };
+export {
+  timezonedb, openweathermap, mapbox, opencagedata,
+};
---
+++ fancy-weather/src/logic/geocoding.js
@@ -0,0 +1,18 @@
+import { opencagedata } from '../SecretAPIKeys';
+
+const getCoordinatesByTown = async (townName, languageCode) => {
+  const query = 'https://api.opencagedata.com/geocode/v1/json?'
+    + `q=${encodeURI(townName)}`
+    + `&key=${opencagedata}`
+    + '&pretty=1&no_annotations=1'
+    + '&limit=1'
+    + `&language=${languageCode}`;
+
+  const response = await fetch(query);
+  const json = await response.json();
+
+  const townData = json.results[0];
+  return { formatted: townData.formatted, geometry: townData.geometry };
+};
+
+export default getCoordinatesByTown;
