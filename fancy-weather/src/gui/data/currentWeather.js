0bdef4b7bf02ce2f102b8d6c8120577568e05d16
ranisans <ranisans@yahoo.com>
2019-12-16 20:39:19 +0300

    fix: add missing weather type

--- fancy-weather/src/gui/data/currentWeather.js
+++ fancy-weather/src/gui/data/currentWeather.js
@@ -4,6 +4,16 @@ const weatherType = {
   Drizzle: { en: 'drizzle', ru: 'мелкий дождь', by: 'імжа' },
   Rain: { en: 'rain', ru: 'ясно', by: 'дождж' },
   Thunderstorm: { en: 'thunderstorm', ru: 'ясно', by: 'навальніца' },
+  Snow: { en: 'snow', ru: 'снег', by: 'снег' },
+  Mist: { en: 'Mist', ru: 'Дымка', by: 'Смуга' },
+  Smoke: { en: 'Smoke', ru: 'Дым', by: 'Дым' },
+  Haze: { en: 'Haze', ru: 'Мгла', by: 'Iмгла' },
+  Dust: { en: 'Dust', ru: 'Пыль', by: 'Пыл' },
+  Fog: { en: 'Fog', ru: 'Туман', by: 'Туман' },
+  Sand: { en: 'Sand', ru: 'Песок', by: 'Пясок' },
+  Ash: { en: 'Ash', ru: 'Пепел', by: 'Пепел' },
+  Squall: { en: 'Squall', ru: 'Шквал', by: 'Шквал' },
+  Tornado: { en: 'Tornado', ru: 'Торнадо', by: 'Тарнада' },
 };
 
 const feelsLike = { en: 'feels like: ', ru: 'по ощущению: ', by: 'адчуваецца як: ' };
